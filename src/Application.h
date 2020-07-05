2879c5732a3aa1adbe80c4d1854306641bbedd00
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-07-05 14:53:24 -0700

    Add new header files

---
+++ src/Application.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Input.h"
+#include "d3d.h"
+#include "ShaderManager.h""
+#include "Timer.h"
+#include "Fps.h"
+#include "Zone.h"
+
+const bool FULL_SCREEN = true;
+const bool VSYNC_ENABLED = true;
+const float SCREEN_DEPTH = 1000.0f;
+const float SCREEN_NEAR = 0.1f;
