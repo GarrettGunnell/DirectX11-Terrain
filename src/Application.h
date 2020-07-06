a3e08f627afaa7308d35bb117a824ea50117999d
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-07-05 17:35:53 -0700

    Stub class definitions

--- src/Application.h
+++ src/Application.h
@@ -11,3 +11,20 @@ const bool FULL_SCREEN = true;
 const bool VSYNC_ENABLED = true;
 const float SCREEN_DEPTH = 1000.0f;
 const float SCREEN_NEAR = 0.1f;
+
+class Application {
+public:
+	Application();
+
+	bool Initialize(HINSTANCE, HWND, int, int);
+	void Shutdown();
+	bool Frame();
+
+private:
+	Input* input;
+	D3D* Direct3D;
+	ShaderManager* shaderManager;
+	Timer* timer;
+	Fps* fps;
+	Zone* zone;
+};
