0ab6dfaabd56cf88492f22c5ab75a9a6f19e92ee
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-08-01 21:41:24 -0700

    Add sphere model files and SkySphere header

---
+++ src/Inc/SkySphere.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <d3d11.h>
+#include <DirectXMath.h>
+#include <fstream>
+
+using namespace DirectX;
+using namespace std;
+
+class SkySphere {
+public:
+	SkySphere();
+
+	bool Initialize(ID3D11Device*);
+	void Shutdown();
+	void Render(ID3D11DeviceContext*);
+
+	int GetIndexCount();
+	XMFLOAT4 GetApexColor();
+	XMFLOAT4 GetCenterColor();
+
+private:
+	int vertexCount, indexCount;
+	ID3D11Buffer* vertexBuffer, *indexBuffer;
+	XMFLOAT4 apexColor, centerColor;
+
+	bool LoadSkySphereModel(char*);
+	void ReleaseSkySphereModel();
+
+	bool InitializeBuffers(ID3D11Device*);
+	void ReleaseBuffers();
+	void RenderBuffers(ID3D11DeviceContext*);
+
+	struct Model {
+		float x, y, z;
+		float tu, tv;
+		float nx, ny, nz;
+	};
+
+	Model* model;
+
+	struct Vertex {
+		XMFLOAT3 position;
+	};
+};
