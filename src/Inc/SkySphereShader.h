0f25ee2b4dad60f829ab9d019d92a21f73d1f920
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-08-02 00:47:11 -0700

    Implement SkySphereShader header

---
+++ src/Inc/SkySphereShader.h
@@ -0,0 +1,38 @@
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+#include <fstream>
+
+using namespace DirectX;
+using namespace std;
+
+class SkySphereShader {
+public:
+	SkySphereShader();
+
+	bool Initialize(ID3D11Device*, HWND);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext*, int, XMMATRIX, XMMATRIX, XMMATRIX, XMFLOAT4, XMFLOAT4);
+
+private:
+	ID3D11VertexShader* vertexShader;
+	ID3D11PixelShader* pixelShader;
+	ID3D11InputLayout* layout;
+	ID3D11Buffer* matrixBuffer;
+	ID3D11Buffer* colorBuffer;
+
+	bool InitializeShader(ID3D11Device*, HWND, WCHAR*, WCHAR*);
+	void ShutdownShader();
+	void OutputShaderErrorMessage(ID3D10Blob*, HWND, WCHAR*);
+	bool SetShaderParameters(ID3D11DeviceContext*, XMMATRIX, XMMATRIX, XMMATRIX, XMFLOAT4, XMFLOAT4);
+	void RenderShader(ID3D11DeviceContext*, int);
+
+	struct MatrixBuffer {
+		XMMATRIX world, view, projection;
+	};
+
+	struct ColorBuffer {
+		XMFLOAT4 apexColor;
+		XMFLOAT4 centerColor;
+	};
+};
